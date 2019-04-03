22ec1e560624685c83aab5b97c7d4beb78ea6907
tetor <tetor.takashi.etou@gmail.com>
2019-04-04 00:21:47 +0900

    min

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use std::f64;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -8,12 +9,17 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
+// #[wasm_bindgen]
+// extern {
+//     fn alert(s: &str);
+// }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, mathetats-wasm!");
+pub fn min(x: f64, y: f64) -> f64 {
+    x.min(y)
 }
+
+// #[wasm_bindgen]
+// pub fn greet() {
+//     alert("Hello, mathetats-wasm!");
+// }
